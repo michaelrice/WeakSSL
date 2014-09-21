7f307de4a219405c28963501fc59489a3e29d06d
Michael Rice <michael@michaelrice.org>
2014-09-21 10:06:51 -0500

    testing some changes to the travis build process

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-grails $UPGRADE --non-interactive && grails test-app unit: --non-interactive --stacktrace
+grails $UPGRADE --non-interactive && grails test-app unit: --non-interactive --stacktrace && grails package-plugin
